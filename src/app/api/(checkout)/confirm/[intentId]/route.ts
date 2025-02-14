41f9d7007b378a857d4b6f6d08b5321dc4704376
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-02-15 05:00:19 +1100

    deploy

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,8 +1,33 @@
-import { prisma } from "@/utils/connect";
-import { NextResponse } from "next/server";
+// import { prisma } from "@/utils/connect";
+// import { NextResponse } from "next/server";
 
-export const PUT = async ({ params }: { params: { intentId: string } }) => {
-  const { intentId } = params;
+// export const PUT = async ({ params }: { params: { intentId: string } }) => {
+//   const { intentId } = params;
+
+//   try {
+//     await prisma.order.update({
+//       where: {
+//         intent_id: intentId,
+//       },
+//       data: { status: "Being prepared!" },
+//     });
+//     return new NextResponse(
+//       JSON.stringify({ message: "Order has been updated" }),
+//       { status: 200 }
+//     );
+//   } catch (err) {
+//     console.log(err);
+//     return new NextResponse(
+//       JSON.stringify({ message: "Something went wrong!" }),
+//       { status: 500 }
+//     );
+//   }
+// };
+import { prisma } from "@/utils/connect";
+import { NextRequest, NextResponse } from "next/server";
+
+export const PUT = async (request: NextRequest) => {
+  const { intentId } = request.params; // Access params from NextRequest
 
   try {
     await prisma.order.update({
@@ -23,5 +48,3 @@ export const PUT = async ({ params }: { params: { intentId: string } }) => {
     );
   }
 };
-
-
