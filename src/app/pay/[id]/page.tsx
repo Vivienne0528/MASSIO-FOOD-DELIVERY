90e9ee057c58dac0652178e4a5c9d32bb5683cca
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-02-15 04:45:37 +1100

    update

---
+++ src/app/pay/[id]/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import CheckoutForm from "@/components/CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { StripeElementsOptions, loadStripe } from "@stripe/stripe-js";
+import { use, useEffect, useState } from "react";
+
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
+);
+
+const PayPage = ({ params }: { params: { id: string } }) => {
+  const [clientSecret, setClientSecret] = useState("");
+
+  //In Next.js, params has become a Promise, which needs to be unwrapped through React.use() to access its properties.
+  //const { id } = params;
+  const { id } = use(params);
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await fetch(
+          `http://localhost:3000/api/create-intent/${id}`,
+          {
+            method: "POST",
+          }
+        );
+        const data = await res.json();
+        setClientSecret(data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    makeRequest();
+  }, [id]);
+
+  const options: StripeElementsOptions = {
+    clientSecret,
+    appearance: {
+      theme: "stripe"
+    }
+  }
+
+  return (
+    <div>
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+    </div>
+  );
+};
+
+export default PayPage;
