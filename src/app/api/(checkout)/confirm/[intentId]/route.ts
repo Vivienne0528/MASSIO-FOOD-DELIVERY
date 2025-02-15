71c55b650082546bf710acd80246ba51894fe244
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-02-15 13:47:43 +1100

    checkout confirm update

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -26,22 +26,24 @@
 import { prisma } from "@/utils/connect";
 import { NextRequest, NextResponse } from "next/server";
 
-export const PUT = async (request: NextRequest) => {
-  const { intentId } = request.params; // Access params from NextRequest
+export const PUT = async (
+  request: NextRequest,
+  { params }: { params: { intentId: string } }
+) => {
+  const { intentId } = params; // ✅ 正确获取路由参数
 
   try {
     await prisma.order.update({
-      where: {
-        intent_id: intentId,
-      },
+      where: { intent_id: intentId },
       data: { status: "Being prepared!" },
     });
+
     return new NextResponse(
       JSON.stringify({ message: "Order has been updated" }),
       { status: 200 }
     );
   } catch (err) {
-    console.log(err);
+    console.error(err);
     return new NextResponse(
       JSON.stringify({ message: "Something went wrong!" }),
       { status: 500 }
